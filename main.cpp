bd0a7cc31c7e706305d5660224ede7134f37abb9
gioauc <giorgio.auciello@marconibari.edu.it>
2024-10-01 07:49:59 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float risp, costomoto, totrisp, ammanco, rimanenza;
+    int count;
+    cout << "Inserisci il costo della moto";
+    cin >> costomoto;
+    cout << "Inerisci l'ammontare dei tuoi risparmi iniziali";
+    cin >> risp;
+    totrisp = risp;
+    while (totrisp < costomoto){
+        ammanco = costomoto - totrisp;
+        cout << "I riparmi (" << totrisp << ") non sono sufficienti. Sono necesari altri" & ammanco;
+        cout << "Quanto altro hai risparmiato?";
+        cin >> risp;
+        count = count + 1;
+        totrisp = totrisp + risp;
+    }
+    cout << "Hai risparmiato " << totrisp << "€. Puoi comprare la moto.";
+    cout << "Hai risparmiato " << count << " volte";
+    rimanenza = totrisp - costomoto;
+    cout << "ti avanzano ancora " << rimanenza;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
